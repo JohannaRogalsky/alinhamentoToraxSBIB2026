6062e02e450ff9b676fe2751dd24ab1b0f59fedb
Johanna <johanna.elisabeth8@gmail.com>
2026-09-21 09:22:36 -0300

    mod ajustando o tamanho do visualizador

--- viewMHDITK.py
+++ viewMHDITK.py
@@ -33,35 +33,45 @@ class MultiPlaneWidget(QWidget):
 		self.update_display()
 
 	def init_ui(self):
-		self.fig = Figure(figsize=(12, 4))
+		self.fig = Figure(figsize=(18, 7))
 		self.canvas = FigureCanvas(self.fig)
 		self.ax_axial = self.fig.add_subplot(1, 3, 1)
 		self.ax_coronal = self.fig.add_subplot(1, 3, 2)
 		self.ax_sagittal = self.fig.add_subplot(1, 3, 3)
+		self.fig.tight_layout()
 
 		self.slider_z = self.make_slider(0, self.z_max - 1, self.z, self.on_z_change)
 		self.slider_y = self.make_slider(0, self.y_max - 1, self.y, self.on_y_change)
 		self.slider_x = self.make_slider(0, self.x_max - 1, self.x, self.on_x_change)
 
-		self.label_z = QLabel()
-		self.label_y = QLabel()
-		self.label_x = QLabel()
+		self.label_z = QLabel(f"Axial (z): {self.z}")
+		self.label_y = QLabel(f"Coronal (y): {self.y}")
+		self.label_x = QLabel(f"Sagital (x): {self.x}")
 
 		sliders_layout = QHBoxLayout()
-		for lbl, sld, lbl_val in [
-			("Axial (z)", self.slider_z, self.label_z),
-			("Coronal (y)", self.slider_y, self.label_y),
-			("Sagital (x)", self.slider_x, self.label_x),
+		sliders_layout.setContentsMargins(4, 0, 4, 0)
+		sliders_layout.setSpacing(12)
+
+		for lbl_widget, sld in [
+			(self.label_z, self.slider_z),
+			(self.label_y, self.slider_y),
+			(self.label_x, self.slider_x),
 		]:
 			col = QVBoxLayout()
-			col.addWidget(QLabel(lbl))
+			col.setContentsMargins(0, 0, 0, 0)
+			col.setSpacing(2)
+			lbl_widget.setStyleSheet("font-size: 10px;")
+			col.addWidget(lbl_widget)
 			col.addWidget(sld)
-			col.addWidget(lbl_val)
 			sliders_layout.addLayout(col)
 
+		sliders_container = QWidget()
+		sliders_container.setLayout(sliders_layout)
+		sliders_container.setMaximumHeight(50)
+
 		main_layout = QVBoxLayout()
-		main_layout.addWidget(self.canvas)
-		main_layout.addLayout(sliders_layout)
+		main_layout.addWidget(self.canvas, stretch=1)
+		main_layout.addWidget(sliders_container, stretch=0)
 		self.setLayout(main_layout)
 
 	def make_slider(self, minimum, maximum, value, callback):
@@ -119,7 +129,7 @@ class MainWindow(QMainWindow):
 		self.current_index = 0
 
 		self.setWindowTitle("Visualizador MHD - Multiplanar")
-		self.resize(1300, 600)
+		self.resize(1800, 850)
 
 		# Lista lateral com os arquivos encontrados (caminho relativo)
 		self.file_list = QListWidget()
